f547f73e332c0c860ecd41578a8164cc96dc8276
nburbinski <32502858+nburbinski@users.noreply.github.com>
2022-01-13 19:51:48 -0500

    Spring Boot Initializer again

---
+++ src/test/java/org/launchcode/VetConnect/VetConnectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.launchcode.VetConnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VetConnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
