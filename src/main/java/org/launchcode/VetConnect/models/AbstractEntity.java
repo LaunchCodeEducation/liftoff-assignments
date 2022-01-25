54dff5101d797aadd5664f216af8762f824d4a53
nburbinski <32502858+nburbinski@users.noreply.github.com>
2022-01-24 19:19:50 -0500

    added abstract entity

---
+++ src/main/java/org/launchcode/VetConnect/models/AbstractEntity.java
@@ -0,0 +1,25 @@
+package org.launchcode.VetConnect.models;
+
+import java.util.Objects;
+
+public abstract class AbstractEntity {
+
+    private int id;
+
+    public int getId() {
+        return id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        AbstractEntity that = (AbstractEntity) o;
+        return id == that.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
