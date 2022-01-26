9df7bacaea3544f5575af202d79ebe1e06d11f10
nburbinski <32502858+nburbinski@users.noreply.github.com>
2022-01-26 12:01:27 -0500

    added signature methods for search

--- src/main/java/org/launchcode/VetConnect/data/ClinicRepository.java
+++ src/main/java/org/launchcode/VetConnect/data/ClinicRepository.java
@@ -4,6 +4,13 @@ import org.launchcode.VetConnect.models.Clinic;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ClinicRepository extends CrudRepository<Clinic, Integer> {
+
+    List<Clinic> findByCity(String city);
+    List<Clinic> findByState(String state);
+
+
 }
