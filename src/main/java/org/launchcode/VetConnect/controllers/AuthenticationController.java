892f3a24ab86422f6af45039d50cf8b7804dba52
nburbinski <32502858+nburbinski@users.noreply.github.com>
2022-01-28 11:42:35 -0500

    Initial setup for authentication

---
+++ src/main/java/org/launchcode/VetConnect/controllers/AuthenticationController.java
@@ -0,0 +1,4 @@
+package org.launchcode.VetConnect.controllers;
+
+public class AuthenticationController {
+}
---
+++ src/main/java/org/launchcode/VetConnect/models/dto/LoginFormDTO.java
@@ -0,0 +1,4 @@
+package org.launchcode.VetConnect.models.dto;
+
+public class LoginFormDTO {
+}
