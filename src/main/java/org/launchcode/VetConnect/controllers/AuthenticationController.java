61c4362cf589e16d87a7db16db09515ef0709510
nburbinski <32502858+nburbinski@users.noreply.github.com>
2022-02-02 17:57:08 -0500

    controller for testing

--- src/main/java/org/launchcode/VetConnect/controllers/AuthenticationController.java
+++ src/main/java/org/launchcode/VetConnect/controllers/AuthenticationController.java
@@ -1,4 +1,14 @@
 package org.launchcode.VetConnect.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class AuthenticationController {
+
+    @GetMapping(value="login")
+    public String login() {
+        return "login";
+    }
+
 }
