c4b7a9e296986f799cddd9601202d1b058c8189c
Kelly Jones <kellymj6@gmail.com>
2022-02-04 13:51:11 -0500

    Added User model

--- src/main/java/org/launchcode/VetConnect/models/User.java
+++ src/main/java/org/launchcode/VetConnect/models/User.java
@@ -1,27 +1,56 @@
 package org.launchcode.VetConnect.models;
 
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import javax.persistence.Entity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
 
 @Entity
 public class User {
 
-    private String username;
+    @NotBlank
+    private String userType;
 
+    @NotBlank
+    private String firstName;
+
+    @NotBlank
+    private String lastName;
+
+    @NotBlank
+    @Email
+    private String emailAddress;
+
+    @NotBlank
     private String pwHash;
 
     private static final BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
 
     public User() {}
 
-    public User(String username, String password) {
-        this.username = username;
+    public User(String userType, String firstName, String lastName, String emailAddress, String password) {
+        this.userType = userType;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.emailAddress = emailAddress;
         this.pwHash = encoder.encode(password);
     }
 
-    public String getUsername() {
-        return username;
+    public String getUserType() {
+        return userType;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
     }
 
     public boolean isMatchingPassword(String password) {
