a9d26bfea93670a7656b350dc7fd975491337ba7
Kelly Jones <kellymj6@gmail.com>
2022-02-04 13:52:31 -0500

    Added RegisterFormDTO

--- src/main/java/org/launchcode/VetConnect/models/dto/RegisterFormDTO.java
+++ src/main/java/org/launchcode/VetConnect/models/dto/RegisterFormDTO.java
@@ -1,4 +1,55 @@
 package org.launchcode.VetConnect.models.dto;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
 public class RegisterFormDTO {
+
+    @NotBlank(message="Please choose a user type.")
+    private String userType;
+
+    @NotBlank(message="First name must not be blank.")
+    private String firstName;
+
+    @NotBlank(message="Last name must not be blank.")
+    private String lastName;
+
+    @NotBlank(message="Email must not be blank.")
+    @Email(message="Enter a valid email address.")
+    private String emailAddress;
+
+    @NotBlank(message="Password must not be blank.")
+    @Size(min = 5, max = 30, message = "Password must be " +
+            "between 5 and 30 characters.")
+    private String password;
+
+    private String verifyPassword;
+
+    // getters
+
+    public String getUserType() {
+        return userType;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getVerifyPassword() {
+        return verifyPassword;
+    }
+
 }
