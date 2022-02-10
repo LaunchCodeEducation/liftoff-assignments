bdc8c8f27b611f02bdadd8e4d66f93b2685b6309
Eric Nguyen <53154237+EricNguyen-B@users.noreply.github.com>
2022-02-10 17:21:34 -0500

    Add AuthenticationController

--- src/main/java/org/launchcode/VetConnect/controllers/AuthenticationController.java
+++ src/main/java/org/launchcode/VetConnect/controllers/AuthenticationController.java
@@ -1,18 +1,132 @@
 package org.launchcode.VetConnect.controllers;
 
+import org.launchcode.VetConnect.models.User;
+import org.launchcode.VetConnect.models.data.UserRepository;
+import org.launchcode.VetConnect.models.dto.LoginFormDTO;
+import org.launchcode.VetConnect.models.dto.RegisterFormDTO;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
+import java.util.Optional;
 
 @Controller
 public class AuthenticationController {
 
+    @Autowired
+    UserRepository userRepository;
+
+    private static final String userSessionKey = "user";
+
+    public User getUserFromSession(HttpSession session) {
+        Integer userId = (Integer) session.getAttribute(userSessionKey);
+        if (userId == null) {
+            return null;
+        }
+
+        Optional<User> user = userRepository.findById(userId);
+
+        if (user.isEmpty()) {
+            return null;
+        }
+
+        return user.get();
+    }
+
+    private static void setUserInSession(HttpSession session, User user) {
+        session.setAttribute(userSessionKey, user.getId());
+    }
+
+    @GetMapping("/register")
+    public String displayRegistrationForm(Model model) {
+        model.addAttribute(new RegisterFormDTO());
+        return "register";
+    }
+
+    @PostMapping("/register")
+    public String processRegistrationForm(@ModelAttribute @Valid RegisterFormDTO registerFormDTO,
+                                          Errors errors, HttpServletRequest request,
+                                          Model model) {
+
+        if (errors.hasErrors()) {
+//            model.addAttribute("title", "Register");
+            return "register";
+        }
+
+        User existingUser = userRepository.findByEmailAddress(registerFormDTO.getEmailAddress());
+
+        if (existingUser != null) {
+            errors.rejectValue("emailAddress", "emailAddress.alreadyexists", "A user with that email already exists");
+//            model.addAttribute("title", "Register");
+            return "register";
+        }
+
+        String password = registerFormDTO.getPassword();
+        String verifyPassword = registerFormDTO.getVerifyPassword();
+        if (!password.equals(verifyPassword)) {
+            errors.rejectValue("password", "passwords.mismatch", "Passwords do not match");
+          //  model.addAttribute("title", "Register");
+            return "register";
+        }
+
+        User newUser = new User(registerFormDTO.getUserType(), registerFormDTO.getFirstName(), registerFormDTO.getLastName(), registerFormDTO.getEmailAddress(), registerFormDTO.getPassword());
+        userRepository.save(newUser);
+        setUserInSession(request.getSession(), newUser);
+
+        return "redirect:";
+    }
+
     @GetMapping(value="login")
-    public String login() {
+    public String displayLoginForm(Model model) {
+        model.addAttribute(new LoginFormDTO());
         return "login";
     }
 
-    @GetMapping(value="register")
-    public String register() {
-        return "register";
+
+    @PostMapping("/login")
+    public String processLoginForm(@ModelAttribute @Valid LoginFormDTO loginFormDTO,
+                                   Errors errors, HttpServletRequest request,
+                                   Model model) {
+
+        if (errors.hasErrors()) {
+            //model.addAttribute("title", "Log In");
+            return "login";
+        }
+
+        User theUser = userRepository.findByEmailAddress(loginFormDTO.getEmailAddress());
+
+        if (theUser == null) {
+            errors.rejectValue("username", "user.invalid", "The given username does not exist");
+            //model.addAttribute("title", "Log In");
+            return "login";
+        }
+
+        String password = loginFormDTO.getPassword();
+
+        if (!theUser.isMatchingPassword(password)) {
+            errors.rejectValue("password", "password.invalid", "Invalid password");
+           // model.addAttribute("title", "Log In");
+            return "login";
+        }
+
+        setUserInSession(request.getSession(), theUser);
+
+        return "redirect:";
     }
+
+    @GetMapping("/logout")
+    public String logout(HttpServletRequest request){
+        request.getSession().invalidate();
+        return "redirect:/login";
+    }
+
+
+
 }
