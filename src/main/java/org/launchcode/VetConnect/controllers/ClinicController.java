477738174227c245cb5fba3446e9814d7cc3ea92
nburbinski <32502858+nburbinski@users.noreply.github.com>
2022-02-11 11:58:20 -0500

    added model attr

--- src/main/java/org/launchcode/VetConnect/controllers/ClinicController.java
+++ src/main/java/org/launchcode/VetConnect/controllers/ClinicController.java
@@ -1,20 +1,37 @@
 package org.launchcode.VetConnect.controllers;
 
+import org.launchcode.VetConnect.models.dto.AddClinicDTO;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
 
 @Controller
 public class ClinicController {
 
     @GetMapping(value = "add")
-    public String addAClinicForm() {
+    public String addAClinicForm(Model model) {
+        model.addAttribute(new AddClinicDTO());
+
         return "add-a-clinic";
     }
 
     @PostMapping(value = "add")
-    public String addAClinicRequest() {
-        return "index";
+    public String addAClinicRequest(@ModelAttribute @Valid AddClinicDTO addClinicDTO, Errors errors, HttpServletRequest request, Model model) {
+        if(errors.hasErrors()) {
+            return "add-a-clinic";
+        }
+
+//        ToDO
+//        Check for existing clinic
+//
+
+        return "redirect:";
     }
 
 }
